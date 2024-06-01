45e5b68e57c6cdcf87c89ad1c5610dd2207ddb83
ksenyshutova <shutovasn@mail.ru>
2024-06-01 17:56:25 +0300

    Final

--- src/__tests__/basic.js
+++ src/__tests__/basic.js
@@ -4,8 +4,7 @@ test.each([
     ['Маг', 90, 'healthy'],
     ['Колдун', 20, 'wounded'],
     ['Ведьмак', 13, 'critical'],
-])
-('testing life of object', (_, health, expected) => {
+])('testing life of object',(_, health, expected) => {
     const result = calculateLife(health);
     expect(result).toBe(expected);
 });
